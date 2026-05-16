d06f5f6f34ceb68e6978a9f19f32d475dacee346
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-05-16 23:18:13 +0530

    Create a program in Python to multiply two numbers

---
+++ Python/005.py
@@ -0,0 +1,7 @@
+# Python program: Multiplication of two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+multiply = a * b
+print("Multiplication is:", multiply)
