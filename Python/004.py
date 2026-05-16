cd14d805b2d3ded6aa1999056c07144b12dc280d
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-05-16 23:13:29 +0530

    Create a program in Python to subtract two numbers

---
+++ Python/004.py
@@ -0,0 +1,7 @@
+# Python program: Subtraction of two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+subtract = a - b
+print("Subtraction is:", subtract)
