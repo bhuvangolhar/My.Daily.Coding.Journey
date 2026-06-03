9dd78a1de7ad86b87fc415979f2e5d0a6732966a
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-06-03 22:58:47 +0530

    Create a C program to check whether an integer is even or odd

---
+++ C/016.c
@@ -0,0 +1,18 @@
+// C program: Even or odd
+
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("Enter an integer = ");
+	scanf("%d",&x);
+
+	if(x%2==0)
+	{
+		printf("%d is even",x);
+	}
+	else
+	{
+		printf("%d is odd",x);
+	}
+}
