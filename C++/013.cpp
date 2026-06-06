e95e46c8118c2650ba3b74f947b0e7ba2300ff39
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-06-06 19:26:01 +0530

    Create a C++ program to find the greater integer

---
+++ C++/013.cpp
@@ -0,0 +1,21 @@
+// C++ program: Greater integer
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a = ";
+	cin>>a;
+	cout<<"Enter the value of b = ";
+	cin>>b;
+
+	if(a>b)
+	{
+		cout<<a<<" is the greater integer";
+	}
+	else
+	{
+		cout<<b<<" is the greater integer";
+	}
+}
