4092f3638f34355bb348d90617527b0d290a53c4
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-06-06 19:27:26 +0530

    Create a C++ program to find the smaller integer

---
+++ C++/014.cpp
@@ -0,0 +1,21 @@
+// C++ program: Smaller integer
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a = ";
+	cin>>a;
+	cout<<"Enter the value of b = ";
+	cin>>b;
+
+	if(a<b)
+	{
+		cout<<a<<" is the smaller integer";
+	}
+	else
+	{
+		cout<<b<<" is the smaller integer";
+	}
+}
