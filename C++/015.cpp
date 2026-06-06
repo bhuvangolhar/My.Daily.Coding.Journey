36ff44cc03b6218972475a32c9f7d6149ee06052
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-06-06 19:28:48 +0530

    Create a C++ program to check whether the given integers are equal or not

---
+++ C++/015.cpp
@@ -0,0 +1,21 @@
+// C++ program: Equal integers
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the value of a = ";
+	cin>>a;
+	cout<<"Enter the value of b = ";
+	cin>>b;
+
+	if(a==b)
+	{
+		cout<<"Both integers are equal";
+	}
+	else
+	{
+		cout<<"The integers are not equal";
+	}
+}
