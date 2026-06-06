b5bdfd74b5609e751a4cad0fdbd8705469b44c06
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-06-06 19:31:15 +0530

    Create a C++ program to check whether an integer is positive or negative

---
+++ C++/016.cpp
@@ -0,0 +1,19 @@
+// C++ program: Positive or negative
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"Enter an integer = ";
+	cin>>x;
+
+	if(x>=0)
+	{
+		cout<<x<<" is positive";
+	}
+	else
+	{
+		cout<<x<<" is negative";
+	}
+}
