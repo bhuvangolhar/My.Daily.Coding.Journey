f1b7389841904a7fbfd54814f3b43a4912e64d48
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-06-06 19:32:51 +0530

    Create a C++ program to check whether an integer is even or odd

---
+++ C++/017.cpp
@@ -0,0 +1,19 @@
+// C++ program: Even or odd
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int y;
+	cout<<"Enter an integer = ";
+	cin>>y;
+
+	if(y%2==0)
+	{
+		cout<<y<<" is even";
+	}
+	else
+	{
+		cout<<y<<" is odd";
+	}
+}
