1d5a7b4164caa0c011e48c7fc30c7e3841d223b1
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-06-19 14:16:10 +0530

    Create a class in Java to check if the given integer is positive, negative or zero

---
+++ Java/022.java
@@ -0,0 +1,29 @@
+// Java class: Positive or negative or zero
+
+package projectFolder;
+
+import java.util.Scanner;
+
+public class PositiveNegativeZero
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter any integer = ");
+        int num = scan.nextInt();
+
+        if (num > 0)
+        {
+            System.out.println("The integer is positive");
+        }
+        else if (num < 0)
+        {
+            System.out.println("The integer is negative");
+        }
+        else
+        {
+            System.out.println("The integer is zero");
+        }
+    }
+}
