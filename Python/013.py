78d54f7599dc3765055c90f88efede6b8470806f
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-07-04 15:04:49 +0530

    Create a program in Python to check whether the given number is even, odd or zero

---
+++ Python/013.py
@@ -0,0 +1,10 @@
+# Python program: Even, odd or zero
+
+num = int(input("Enter any number: "))
+
+if num == 0:
+    print("The given number is zero")
+elif num % 2 == 0:
+    print("The given number is even")
+else:
+    print("The given number is odd")
