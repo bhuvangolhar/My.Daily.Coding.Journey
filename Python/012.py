74604445c0801828b0c880e9d53debb2708e1938
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-07-04 14:59:48 +0530

    Create a program in Python to check whether the given number is positive or negative

---
+++ Python/012.py
@@ -0,0 +1,8 @@
+# Python program: Positive or negative
+
+number = int(input("Enter any number: "))
+
+if number > 0:
+    print("The given number is positive")
+else:
+    print("The given number is negative")
