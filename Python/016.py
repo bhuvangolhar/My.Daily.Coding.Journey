bdad9f5e55adbf53e7780d726e49659fd1e84680
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-07-04 15:09:29 +0530

    Create a program in Python to check the pass or fail result

---
+++ Python/016.py
@@ -0,0 +1,8 @@
+# Python program: Pass or fail
+
+marks = int(input("Enter your total marks: "))
+
+if marks >= 35:
+    print("You have passed the examination")
+else:
+    print("You have failed the examination")
