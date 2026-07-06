0c812e2ef7f9280d274a7794f0e1f26a645f5817
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-07-06 15:23:32 +0530

    Create a program in Python to check whether the given number is divisible by 7 or not

---
+++ Python/022.py
@@ -0,0 +1,8 @@
+# Python program: Divisible by 7
+
+num = int(input("Enter any number: "))
+
+if num % 7 == 0:
+    print("The given number is divisible by 7")
+else:
+    print("The given number is not divisible by 7")
