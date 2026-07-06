e4612813318b9d95317a7a418b493488c48b6ada
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-07-06 15:45:19 +0530

    Create a program in Python to check whether there is a profit or loss

---
+++ Python/025.py
@@ -0,0 +1,9 @@
+# Python program: Profit or loss
+
+cp = int(input("Enter the cost price: "))
+sp = int(input("Enter the selling price: "))
+
+if sp > cp:
+    print("There is a profit")
+else:
+    print("There is a loss")
