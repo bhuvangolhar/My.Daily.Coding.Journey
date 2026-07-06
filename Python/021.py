6bd76f06faa3462ff59df4ff3d5c9abecef51fab
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-07-06 15:17:52 +0530

    Create a program in Python to check the driving license eligibility

---
+++ Python/021.py
@@ -0,0 +1,8 @@
+# Python program: Driving license
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible for a driving license")
+else:
+    print("You are not eligible for a driving license")
