78e7ace4ad6bfc0d3cd9b1c4fd12a59c739995a6
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-07-06 15:30:15 +0530

    Create a program in Python to check whether the given year is a leap year or not

---
+++ Python/023.py
@@ -0,0 +1,8 @@
+# Python program: Leap year
+
+year = int(input("Enter any year: "))
+
+if year % 4 == 0:
+    print("The given year is a leap year")
+else:
+    print("The given year is not a leap year")
