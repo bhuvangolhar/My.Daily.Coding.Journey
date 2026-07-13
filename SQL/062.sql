82ad1ee11b3c85c36f3eb3b948354736e08938d1
Bhuvan Golhar <bhuvangolhar2025@gmail.com>
2026-07-13 22:11:10 +0530

    Write a query in MySQL to create a table with a default constraint

---
+++ SQL/062.sql
@@ -0,0 +1,9 @@
+-- MySQL query: Default constraint
+
+CREATE TABLE Users
+(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    age INT DEFAULT 22
+);
